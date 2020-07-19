a183cc71ef5220b02a7af12bff7f95cab574447e
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-07-19 11:33:08 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     // isotope
     let $btns = $(".project-area .button-group button");
     $btns.click(function (e) {
-        $(".project-area .button-group button").removeClass("active");
+        // $(".project-area .button-group button").removeClass("active");
 
         
-        e.target.classList.add("active");
+        // e.target.classList.add("active");
 
         let selector = $(e.target).attr("data-filter");
         $(".project-area .grid").isotope({
@@ -66,160 +66,162 @@ $(document).ready(function () {
     // skrollr plugin
     // var s = skrollr.init();
 
+    
     // fancybox plugin options
-    $("[data-fancybox]").fancybox({
-        loop: true,
-    });
 
-    $("#dassi").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/dassi",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
-    $("#juksy").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/juksy/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
-    $("#sweetme").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/sweetme/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
-    $("#fullPageWeb").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/fullPageWeb/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
-    $("#print").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/print/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
-    $("#yellow-studio").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/yellow-studio/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("[data-fancybox]").fancybox({
+    //     loop: true,
+    // });
 
-    $("#form-animation").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/form-animation",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#dassi").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/dassi",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+    // $("#juksy").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/juksy/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+    // $("#sweetme").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/sweetme/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+    // $("#fullPageWeb").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/fullPageWeb/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+    // $("#print").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/print/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+    // $("#yellow-studio").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/yellow-studio/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#camera").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/camera--animation/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#form-animation").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/form-animation",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#shopping-list").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/jquery-json-shopping-list",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#camera").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/camera--animation/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#vue-js-piano").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/piano-vue.js",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#shopping-list").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/jquery-json-shopping-list",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#event-trigger").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/piano-vue.js",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#vue-js-piano").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/piano-vue.js",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#feedback-send-mailr").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/feedback-send-mail",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#event-trigger").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/piano-vue.js",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 
-    $("#cutover-phone").on("click", function () {
-        $.fancybox.open({
-            src: "https://hardy0529.github.io/jquery--cutover-phone/",
-            type: "iframe",
-            opts: {
-                afterShow: function (instance, current) {
-                    console.info("done!");
-                },
-            },
-        });
-    });
+    // $("#feedback-send-mailr").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/feedback-send-mail",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
+
+    // $("#cutover-phone").on("click", function () {
+    //     $.fancybox.open({
+    //         src: "https://hardy0529.github.io/jquery--cutover-phone/",
+    //         type: "iframe",
+    //         opts: {
+    //             afterShow: function (instance, current) {
+    //                 console.info("done!");
+    //             },
+    //         },
+    //     });
+    // });
 });
 
